f9ea29c7b6d2fd4b9123a63abd41e4d15a94f792
sakshambhalla <sakshambhalla11@gmail.com>
2018-04-05 02:29:19 +0530

    Adding new Table

--- database.js
+++ database.js
@@ -17,7 +17,7 @@ function connect(){
 }
 
 function getAllUsers(users){
-    connection.query('Select * from UserData', function(err,data){
+    connection.query('Select * from UserDetails', function(err,data){
         users(data);
     });
 }
@@ -28,14 +28,14 @@ function addUser(details){
     let hash = bcrypt.hashSync(details.password, salt);
 
 
-    connection.query(`Create table if not exists UserData(
+    connection.query(`Create table if not exists UserDetails(
                                 id int primary key AUTO_INCREMENT,
                                 firstname varchar(30),
                                 lastname varchar(30),
                                 username varchar(40),
                                 password varchar(100)
                                 ); 
-                                Insert into UserData(firstname,lastname,username,password) values('${details.firstname}','${details.lastname}','${details.username}','${hash}');`, function(err,data){
+                                Insert into UserDetails(firstname,lastname,username,password) values('${details.firstname}','${details.lastname}','${details.username}','${hash}');`, function(err,data){
                 if(err) throw err;
                 console.log(hash);
     });
